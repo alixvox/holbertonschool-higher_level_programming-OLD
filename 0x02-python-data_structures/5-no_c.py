ebc43cc10a7c0234605da801dedee7856842c119
alixvox <alixvox@gmail.com>
2022-01-06 21:47:07 -0800

    fixing

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace("C", "")
-    new_string = new_string.replace("c", "")
+    new_string = my_string.replace("C", None)
+    new_string = new_string.replace("c", None)
     return new_string

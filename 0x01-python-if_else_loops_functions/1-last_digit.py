24161c573420ed343d25d9996def3634219735f2
alixvox <alixvox@gmail.com>
2022-01-07 18:10:07 -0800

    Fixing uppercase!

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 abs_num = abs(number)
 last_dig = abs_num % 10
+print("Last digit of {:d} is {:d} ".format(number, last_dig), end='')
 if last_dig < 6 and last_dig != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_dig))
+    print("and is less than 6 and not 0")
 elif last_dig > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_dig))
+    print("and is greater than 5")
 else:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_dig))
+    print("and is 0")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str) - 1):
-        if ord(str[i]) > 96 and ord(str[i]) < 123:
-            print(chr(ord(str[i]) - 32), end='')
-        else:
-            print(str[i], end='')
+        is_lower = ord(str[i]) > 96 and ord(str[i]) < 123
+        print(chr(ord(str[i]) - 32) if is_lower else str[i], end='')
     i += 1
-    if ord(str[i]) > 96 and ord(str[i]) < 123:
-        print(chr(ord(str[i]) - 32))
-    else:
-        print(str[i])
+    is_lower = ord(str[i]) > 96 and ord(str[i]) < 123
+    print(chr(ord(str[i]) - 32) if is_lower else str[i])

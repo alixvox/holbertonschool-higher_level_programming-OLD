752b12fb69156adfae7144ff54088ae87e95b49d
alixvox <alixvox@gmail.com>
2022-01-07 17:52:27 -0800

    Fixing uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(str.upper())
+    for i in range(0, len(str) - 1):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            print(chr(ord(str[i]) - 32), end='')
+        else:
+            print(str[i], end='')
+    i += 1
+    if ord(str[i]) > 96 and ord(str[i]) < 123:
+        print(chr(ord(str[i]) - 32))
+    else:
+        print(str[i])

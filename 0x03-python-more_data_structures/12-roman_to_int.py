ab2d51c7ee2b7f6391e2d28f611780c65e7c3fec
alixvox <alixvox@gmail.com>
2022-01-10 19:53:51 -0800

    Roman fix2

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -27,46 +27,4 @@ def roman_to_int(roman_string):
             idx += 1
             if idx == len(roman_list):
                 break
-
-
-
-    """
-    for rom in roman_string:
-        if rom == 'M':
-            number = number + 1000
-        elif rom == 'C':
-            if roman_list[idx + 1] == 'D':
-                number += 400
-                idx += 1
-            else:
-                number += 100
-        elif rom == 'D':
-            if roman_list[idx - 1] == 'C' and idx != 0:
-                continue
-            else:
-                number += 500
-        elif rom == 'X':
-            if roman_list[idx + 1] == 'L':
-                number += 40
-                idx += 1
-            else:
-                number += 10
-        elif rom == 'L':
-            if roman_list[idx - 1] == 'X' and idx != 0:
-                continue
-            else:
-                number += 50
-        elif rom == 'I':
-            if roman_list[idx + 1] == 'V':
-                number += 4
-                idx += 1
-            else:
-                number += 1
-        elif rom == 'V':
-            if roman_list[idx - 1] == 'I' and idx != 0:
-                continue
-            else:
-                number += 5
-        idx += 1
-        """
     return number

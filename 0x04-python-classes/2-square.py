773cd02f309da47ab3634c94625cc0ee015ae6ee
alixvox <alixvox@gmail.com>
2022-01-11 15:17:47 -0800

    ep8 validation

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module creates a class called Square."""
+
+
 class Square:
-    """
-    This class creates a Square.
+    """This class creates a Square.
     Attributes:
-        __size (int): Size of the square
-    """
+        __size (int): Size of the square"""
+
     def __init__(self, size=0):
         """Intializes a Square with int size greater
         than or equal to zero"""
@@ -14,6 +15,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Computes the area of a square using it's size"""
         return self.__size * self.__size
--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,10 +1,11 @@
 """This module creates a class called Square."""
+
+
 class Square:
-    """
-    This class creates a Square.
+    """This class creates a Square.
     Attributes:
-        __size (int): Size of the square
-    """
+        __size (int): Size of the square"""
+
     def __init__(self, size=0):
         """Intializes a Square with int size greater
         than or equal to zero"""
@@ -13,9 +14,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, size):
         if not type(size) is int:
@@ -23,5 +26,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size * self.__size

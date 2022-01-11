50eefa192100df1616f422428ceb4d93785b204f
alixvox <alixvox@gmail.com>
2022-01-10 19:52:50 -0800

    Roman fix

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
     roman_list = list(roman_string)
     numerals = [
         ('I', 1, None, None),

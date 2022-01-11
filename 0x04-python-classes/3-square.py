7f5f156c2fb7eae06f40cb2d7e510047cf7087ca
alixvox <alixvox@gmail.com>
2022-01-11 14:47:54 -0800

    objects/classes

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, size):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size

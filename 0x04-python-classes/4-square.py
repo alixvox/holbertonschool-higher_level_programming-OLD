0d9cb0645c7ceabbded5df1fec6c9571587d01d1
alixvox <alixvox@gmail.com>
2022-01-11 15:21:21 -0800

    final change

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -32,6 +32,8 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
+        if self.__size == -0:
+            print()
         for i in range(self.__size):
             for i in range(self.__size):
                 print("#", end='')

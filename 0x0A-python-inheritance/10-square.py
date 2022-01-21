fe6c2e8e6970cd92cde97b7f57b88cd83347f098
alixvox <alixvox@gmail.com>
2022-01-20 21:59:47 -0800

    alast task

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains class Square.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class is based on Rectanlge from
+    the module 9-rectangle.py:
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    def __init__(self, size):
+        """This method instantiates with size:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        if self.integer_validator("size", size):
+            self.__width = size
+            self.__height = size
+            self.__size = size
+
+    def __str__(self):
+        """This method prints with size:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """This method returns area with size:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        return self.__size * self.__size

c4b06448cec8d7a4cd5479a477bc8b1748ac6224
alixvox <alixvox@gmail.com>
2022-01-21 21:29:01 -0800

    fixing task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains class Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class is based on Rectanlge from
+    the module 9-rectangle.py:
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    def __init__(self, size):
+        """This method instantiates with size:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        self.integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+        self.__size = size
+
+    def area(self):
+        """This method returns area with size:
+        python3 -c 'print(__import__("my_module").
+        MyClass.my_function.__doc__)'"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Square.__str__() uses this format:
+        [Square] <size>/<size>
+        """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)

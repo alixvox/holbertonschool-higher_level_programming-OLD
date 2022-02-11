f8507d882acfc8ad1b46bc65e3fdbbf31b9d5e4e
alixvox <alixvox@gmail.com>
2022-02-10 17:24:35 -0800

    2nd SQL proj

--- 0x01-SQL_more_queries/14-my_genres.sql
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,5 @@
+-- uses the hbtn_0d_tvshows database to list
+-- all genres of the show Dexter.
 SELECT tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
--- 0x01-SQL_more_queries/15-comedy_only.sql
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,4 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
